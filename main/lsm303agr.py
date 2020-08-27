bc5742dae87f673e6c29f7d387aa0746a9c05c13
Ben Sherlock <benjamin.sherlock@ncl.ac.uk>
2020-08-27 13:30:50 +0100

    Working magnetometer data.

--- main/lsm303agr.py
+++ main/lsm303agr.py
@@ -32,7 +32,7 @@
 
 # Default I2C Addresses
 _ACCEL_I2C_ADDR = 0b0011001
-_MAG_I2C_ADDR = 0b0011110
+_MAGNETO_I2C_ADDR = 0b0011110
 
 
 # Registers and addresses from https://www.st.com/resource/en/datasheet/lsm303agr.pdf
@@ -120,10 +120,23 @@ ACCEL_ODR_400HZ                 = 0b0111
 ACCEL_ODR_1620HZ                = 0b1000
 ACCEL_ODR_1344HZ                = 0b1001
 
+# Full Scale
 ACCEL_FS_2G                     = 0b00
 ACCEL_FS_4G                     = 0b01
 ACCEL_FS_8G                     = 0b10
 ACCEL_FS_16G                    = 0b11
+
+# Magnetometer System Mode, see Table 94.
+MAGNETO_MD_CONTINUOUS           = 0b00
+MAGNETO_MD_SINGLE               = 0b01
+MAGNETO_MD_IDLE0                = 0b10
+MAGNETO_MD_IDLE1                = 0b11
+
+# Magnetometer Output Data Rate
+MAGNETO_ODR_10HZ                = 0b00
+MAGNETO_ODR_20HZ                = 0b01
+MAGNETO_ODR_50HZ                = 0b10
+MAGNETO_ODR_100HZ               = 0b11
 
 
 class LSM303AGR:
@@ -133,9 +146,9 @@ class LSM303AGR:
     # Accelerometer default 7-bit address: 0011001b 0x19
     # Magnetometer default 7-bit address:  0011110b 0x1E
 
-    def __init__(self, i2c=None, accel_i2c_address=_ACCEL_I2C_ADDR, mag_i2c_address=_MAG_I2C_ADDR):
+    def __init__(self, i2c=None, accel_i2c_address=_ACCEL_I2C_ADDR, magneto_i2c_address=_MAGNETO_I2C_ADDR):
         self._accel_i2c_address = accel_i2c_address
-        self._mag_i2c_address = mag_i2c_address
+        self._magneto_i2c_address = magneto_i2c_address
         self._i2c = i2c
 
     def __call__(self):
@@ -149,7 +162,7 @@ class LSM303AGR:
             return False
         whoami_a = the_bytes[0]
 
-        the_bytes = self._read_byte_from_register(_MAG_I2C_ADDR, _REG_WHO_AM_I_M)
+        the_bytes = self._read_byte_from_register(_MAGNETO_I2C_ADDR, _REG_WHO_AM_I_M)
         if not the_bytes:
             return False
         whoami_m = the_bytes[0]
@@ -222,10 +235,8 @@ class LSM303AGR:
         odr = (reg_val >> 4) & 0x0F
         return reg_val, xen, yen, zen, lpen, odr
 
-
     def set_accel_ctrl_reg4(self, hr=0, accel_fs=ACCEL_FS_2G, bdu=0):
         """Set Accelerometer CTRL_REG4 values."""
-        #reg_val = ((accel_odr & 0x0F) << 4) | ((lpen & 0x01) << 3) | ((zen & 0x01) << 2) | ((yen & 0x01) << 1) | (xen & 0x01)
         reg_val = ((bdu & 0x01) << 7) | ((accel_fs & 0x03) << 4) | ((hr & 0x01) << 3)
         self._write_bytes_to_register(self._accel_i2c_address, _REG_CTRL_REG4_A, bytes([reg_val]))
 
@@ -237,7 +248,6 @@ class LSM303AGR:
         fs = (reg_val >> 4) & 0x03
         bdu = (reg_val >> 7) & 0x01
         return reg_val, hr, fs, bdu
-
 
     def get_accel_status_reg(self):
         """Get the Accelerometer STATUS_REG values."""
@@ -253,7 +263,6 @@ class LSM303AGR:
         zyxor = (reg_val >> 7) & 0x01
         return reg_val, xda, yda, zda, zyxda, xor, yor, zor, zyxor
 
-
     def get_accel_outputs(self):
         """Get Accelerometer outputs for X, Y and Z axis from the OUT_X_L_A to OUT_Z_H_A registers.
         Returns x, y, z as int16 direct from the registers."""
@@ -264,15 +273,83 @@ class LSM303AGR:
 
         return x_val, y_val, z_val
 
-
-
     # Magnetometer
-    # ...
 
+    def set_magneto_cfg_reg_a(self, magneto_md=MAGNETO_MD_CONTINUOUS, magneto_odr=MAGNETO_ODR_10HZ, lp=0, soft_rst=0, reboot=0, comp_temp_en=0):
+        """Set Magnetometer CFG_REG_A values."""
+        reg_val = ((comp_temp_en & 0x01) << 7) | ((reboot & 0x01) << 6) | ((soft_rst & 0x01) << 5) | ((lp & 0x01) << 4) | ((magneto_odr & 0x03) << 2) | (magneto_md & 0x03)
+        self._write_bytes_to_register(self._magneto_i2c_address, _REG_CFG_REG_A_M, bytes([reg_val]))
+        return None
 
+    def get_magneto_cfg_reg_a(self):
+        """Get the Magnetometer CFG_REG_A values."""
+        the_bytes = self._read_bytes_from_register(self._magneto_i2c_address, _REG_CFG_REG_A_M)
+        reg_val = the_bytes[0]
+        md = reg_val & 0x03
+        odr = (reg_val >> 2) & 0x03
+        lp = (reg_val >> 4) & 0x01
+        soft_rst = (reg_val >> 5) & 0x01
+        reboot = (reg_val >> 6) & 0x01
+        comp_temp_en = (reg_val >> 7) & 0x01
+        return reg_val, md, odr, lp, soft_rst, reboot, comp_temp_en
 
+    def set_magneto_cfg_reg_b(self, lpf=0, off_canc=0, set_freq=0, int_on_dataoff=0, off_canc_one_shot=0):
+        """Set Magnetometer CFG_REG_B values."""
+        reg_val = ((off_canc_one_shot & 0x01) << 4) | ((int_on_dataoff & 0x01) << 3) | ((set_freq & 0x01) << 2) | ((off_canc & 0x01) << 1) | (lpf & 0x01)
+        self._write_bytes_to_register(self._magneto_i2c_address, _REG_CFG_REG_B_M, bytes([reg_val]))
+        return None
 
+    def get_magneto_cfg_reg_b(self):
+        """Get the Magnetometer CFG_REG_B values."""
+        the_bytes = self._read_bytes_from_register(self._magneto_i2c_address, _REG_CFG_REG_B_M)
+        reg_val = the_bytes[0]
+        lpf = reg_val & 0x01
+        off_canc = (reg_val >> 1) & 0x01
+        set_freq = (reg_val >> 2) & 0x01
+        int_on_dataoff = (reg_val >> 3) & 0x01
+        off_canc_one_shot = (reg_val >> 4) & 0x01
+        return reg_val, lpf, off_canc, set_freq, int_on_dataoff, off_canc_one_shot
 
+    def set_magneto_cfg_reg_c(self, int_mag=0, self_test=0, ble=0, bdu=0, int_mag_pin=0):
+        """Set Magnetometer CFG_REG_C values."""
+        reg_val = ((int_mag_pin & 0x01) << 6) | ((bdu & 0x01) << 4) | ((ble & 0x01) << 3) | ((self_test & 0x01) << 1) | (int_mag & 0x01)
+        self._write_bytes_to_register(self._magneto_i2c_address, _REG_CFG_REG_C_M, bytes([reg_val]))
+        return None
+
+    def get_magneto_cfg_reg_c(self):
+        """Get the Magnetometer CFG_REG_C values."""
+        the_bytes = self._read_bytes_from_register(self._magneto_i2c_address, _REG_CFG_REG_C_M)
+        reg_val = the_bytes[0]
+        int_mag = reg_val & 0x01
+        self_test = (reg_val >> 1) & 0x01
+        ble = (reg_val >> 3) & 0x01
+        bdu = (reg_val >> 4) & 0x01
+        int_mag_pin = (reg_val >> 6) & 0x01
+        return reg_val, int_mag, self_test, ble, bdu, int_mag_pin
+
+    def get_magneto_status_reg(self):
+        """Get the Magnetometer STATUS_REG values."""
+        the_bytes = self._read_bytes_from_register(self._magneto_i2c_address, _REG_STATUS_REG_M)
+        reg_val = the_bytes[0]
+        xda = reg_val & 0x01
+        yda = (reg_val >> 1) & 0x01
+        zda = (reg_val >> 2) & 0x01
+        zyxda = (reg_val >> 3) & 0x01
+        xor = (reg_val >> 4) & 0x01
+        yor = (reg_val >> 5) & 0x01
+        zor = (reg_val >> 6) & 0x01
+        zyxor = (reg_val >> 7) & 0x01
+        return reg_val, xda, yda, zda, zyxda, xor, yor, zor, zyxor
+
+    def get_magneto_outputs(self):
+        """Get Magnetometer outputs for X, Y and Z axis from the OUTX_L_REG_M to OUTZ_H_REG_M registers.
+        Returns x, y, z as int16 direct from the registers."""
+        the_bytes = self._read_bytes_from_register(self._magneto_i2c_address, _REG_OUTX_L_REG_M, 6)
+        x_val = self._bytes_to_int16(the_bytes[0], the_bytes[1])
+        y_val = self._bytes_to_int16(the_bytes[2], the_bytes[3])
+        z_val = self._bytes_to_int16(the_bytes[4], the_bytes[5])
+
+        return x_val, y_val, z_val
 
     # Reading and Writing to Registers. I2C See page 38 of datasheet.
     def _read_bytes_from_register(self, i2c_address: int, register_address: int, num_bytes: int = 1) -> bytes:
@@ -285,7 +362,6 @@ class LSM303AGR:
 
         # return self._i2c.readfrom_mem(i2c_address, the_reg_address_with_flags, num_bytes) # machine.I2C
         return self._i2c.mem_read(num_bytes, i2c_address, the_reg_address_with_flags)  # pyb.I2C
-
 
     def _write_bytes_to_register(self, i2c_address: int, register_address: int, the_bytes: bytes):
         """Write the_bytes to a starting register.
@@ -335,46 +411,49 @@ class LSM303AGR:
 
         return val
 
-
-    def ben_debug(self):
+    def run_example(self):
+        """A simple example showing how to initialise and run the LSM303AGR for
+        Accelerormeter, Magnetometer, and Temperature readings."""
         the_bytes = self._read_bytes_from_register(_ACCEL_I2C_ADDR, _REG_WHO_AM_I_A)
         print("WHO_AM_I_A=" + str(the_bytes[0]))
 
-        the_bytes = self._read_bytes_from_register(_MAG_I2C_ADDR, _REG_WHO_AM_I_M)
+        the_bytes = self._read_bytes_from_register(_MAGNETO_I2C_ADDR, _REG_WHO_AM_I_M)
         print("WHO_AM_I_M=" + str(the_bytes[0]))
 
         the_bytes = self._read_bytes_from_register(self._accel_i2c_address, _REG_CTRL_REG1_A)
         print("CTRL_REG1_A=" + str(the_bytes[0]))
 
-        import pyb
-
         # Temperature
         self.set_accel_ctrl_reg4(bdu=1)
         self.set_temp_cfg_reg(en=1)
 
-
         # Accelerometer
         self.set_accel_ctrl_reg1(accel_odr=ACCEL_ODR_1HZ)
-        self.set_accel_ctrl_reg4(accel_fs=ACCEL_FS_2G, hr=0, bdu=1) # bdu=1 needed for temperature
+        self.set_accel_ctrl_reg4(accel_fs=ACCEL_FS_2G, hr=0, bdu=1)  # bdu=1 needed for temperature
+
+        # Magnetometer
+        self.set_magneto_cfg_reg_a(magneto_odr=MAGNETO_ODR_10HZ, magneto_md=MAGNETO_MD_CONTINUOUS, lp=0, comp_temp_en=1)
+        self.set_magneto_cfg_reg_b(lpf=1)
+        self.set_magneto_cfg_reg_c(bdu=1)
 
         while True:
-
             # Check the temperature status register for new data
             status_reg, tda, tor = self.get_temp_status_reg_aux()
             if tda:
                 temperature = self.get_temp_output_float()
                 print("temperature=" + str(temperature))
 
-
-            # Check the status register for new data
+            # Check the status register for new accelerometer data
             status_reg, xda, yda, zda, zyxda, xor, yor, zor, zyxor = self.get_accel_status_reg()
             if zyxda:
                 x_val, y_val, z_val = self.get_accel_outputs()
-                print("x_val=" + str(x_val) + " y_val=" + str(y_val) + " z_val=" + str(z_val))
+                print("Accel: x_val=" + str(x_val) + " y_val=" + str(y_val) + " z_val=" + str(z_val))
 
-
-
-
+            # Check the status register for new magnetometer data
+            status_reg, xda, yda, zda, zyxda, xor, yor, zor, zyxor = self.get_magneto_status_reg()
+            if zyxda:
+                x_val, y_val, z_val = self.get_magneto_outputs()
+                print("Magneto: x_val=" + str(x_val) + " y_val=" + str(y_val) + " z_val=" + str(z_val))
 
 
 def main():
@@ -388,11 +467,10 @@ def main():
     pyb.Pin('PULL_SDA', pyb.Pin.OUT, value=1)  # enable 5.6kOhm X10/SDA pull-up
     # i2c = machine.I2C(1, freq=400000)  # machine.I2C
     i2c = pyb.I2C(1) # pyb.I2C
-    i2c.init(pyb.I2C.MASTER, baudrate=400000) # pyb.I2C
+    i2c.init(pyb.I2C.MASTER, baudrate=400000)  # pyb.I2C
 
-
-    sensor=LSM303AGR(i2c)
-    sensor.ben_debug()
+    sensor = LSM303AGR(i2c)
+    sensor.run_example()
 
 
 if __name__ == '__main__':
